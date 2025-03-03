7c7b056ea70bb110643e408ffe6bfe3419691a5b
Harshyadav812 <raoharsh138@gmail.com>
2025-03-03 23:26:29 +0000

    feat:Add queries for managing Pokemon and trainers
    - Implement addPokemon to insert new Pokemon with
    validation
    - Implement deletePokemonById to remove a Pokemon
    by ID
    - Implement deleteTrainerById to remove trainer by
    ID

--- db/queries.js
+++ db/queries.js
@@ -44,10 +44,13 @@ async function getAllTrainers() {
 }
 
 async function getTrainerById(id) {
-  const query = `SELECT * FROM trainers WHERE id = $1`;
+  const query = `SELECT pokemons.pokemon_name, pokemons.image_path AS pokemon_image, trainers.id, trainers.trainer_name, trainers.description, trainers.image_path AS trainer_image  
+  FROM trainers
+  JOIN pokemons ON trainers.id = pokemons.trainer_id
+  WHERE trainers.id = $1`;
   try {
     const res = await pool.query(query, [id]);
-    return res.rows[0];
+    return res.rows;
   } catch (err) {
     console.error("Error fetching trainer by ID: ", err);
     throw err;
@@ -82,4 +85,38 @@ async function getPokemonsByType(type_id) {
   }
 }
 
-module.exports = { getAllPokemons, getPokemonById, getAllTrainers, getTrainerById, getAllTypes, getPokemonsByType };
+async function addPokemon(pokemon_name, description, image_path, trainer_name, type_name) {
+  try {
+    const trainerQuery = "SELECT id FROM trainers WHERE trainer_name = $1";
+    const trainerRes = await pool.query(trainerQuery, [trainer_name])
+    if (trainerRes.rows.length === 0) throw new Error("Trainer not found!")
+    const trainer_id = trainerRes.rows[0].id;
+
+    const typeQuery = "SELECT id FROM types WHERE type_name = $1";
+    const typeRes = await pool.query(typeQuery, [type_name]);
+    if (typeRes.rows.length === 0) throw new Error("Type not found!");
+    const type_id = typeRes.rows[0].id;
+
+    const insertQuery = `INSERT INTO pokemons (pokemon_name, description, image_path, trainer_id, type_id) 
+    VALUES($1, $2, $3, $4, $5)
+    RETURNING *`;
+    const res = await pool.query(insertQuery, [pokemon_name, description, image_path, trainer_id, type_id]);
+    return res.rows[0];
+  } catch (err) {
+    console.error("Error adding Pokemon: ", err);
+    throw err;
+  }
+}
+
+async function deletePokemonById(id) {
+  const deleteQuery = "DELETE FROM pokemons WHERE id = $1";
+  await pool.query(deleteQuery, [id]);
+}
+
+async function deleteTrainerById(id) {
+  const deleteTrianerQuery = "DELETE FROM trainers WHERE id = $1";
+  await pool.query(deleteTrianerQuery, [id]);
+
+}
+
+module.exports = { getAllPokemons, getPokemonById, getAllTrainers, getTrainerById, getAllTypes, getPokemonsByType, addPokemon, deletePokemonById, deleteTrainerById };
