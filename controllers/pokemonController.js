ce147e33930a56f6b7c391e5149f1469b024bf21
Harshyadav812 <raoharsh138@gmail.com>
2025-03-03 23:19:14 +0000

    feat: Add Pokemon controller with image upload handling
    - Implement getAllPokemons controller to display
    Pokemon list
    - Add getPokemonById for viewing Pokemon details
    - Add pokemonCreateGet controller to display
    creation form
    - Implement pokemonCreatePost with image file
    processing
    - Add deletePokemonById controller for Pokemon
    removal

--- controllers/pokemonController.js
+++ controllers/pokemonController.js
@@ -17,5 +17,39 @@ async function getPokemonById(req, res) {
   })
 }
 
-module.exports = { getAllPokemons, getPokemonById }
+async function pokemonCreateGet(req, res) {
+  const types = await db.getAllTypes();
+  const trainers = await db.getAllTrainers();
+  res.render("addPokemonForm", {
+    types,
+    trainers
+  })
+}
+
+async function pokemonCreatePost(req, res) {
+  const { pokemon_name, description, trainer_name, type_name } = req.body;
+  const image_file = req.file;
+
+  if (!image_file) {
+    return res.status(400).send("No image file uploaded or invalid file type.");
+  }
+
+  const image_path = `/assets/pokemon-img/${image_file.filename}`
+
+  try {
+    await db.addPokemon(pokemon_name, description, image_path, trainer_name, type_name);
+    res.redirect('/pokemons');
+  } catch (err) {
+    res.status(500).send("Error adding pokemon: " + err.message);
+  }
+
+}
+
+async function deletePokemonById(req, res) {
+  const id = req.params.id;
+  await db.deletePokemonById(id);
+  res.redirect('/pokemons')
+}
+
+module.exports = { getAllPokemons, getPokemonById, pokemonCreateGet, pokemonCreatePost, deletePokemonById }
 // getPokemonById
