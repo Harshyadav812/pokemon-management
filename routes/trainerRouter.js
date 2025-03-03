b17ba1be7fb8856ab364bf2fada469fe1c6dab7d
Harshyadav812 <raoharsh138@gmail.com>
2025-03-03 23:34:41 +0000

    Implement route to delete a trainer by ID

--- routes/trainerRouter.js
+++ routes/trainerRouter.js
@@ -4,5 +4,6 @@ const trainerRouter = Router();
 
 trainerRouter.get("/", trainerController.getAllTrainers);
 trainerRouter.get("/:id", trainerController.getTrainerById);
+trainerRouter.get("/:id/delete", trainerController.deleteTrainerById);
 
 module.exports = trainerRouter;
